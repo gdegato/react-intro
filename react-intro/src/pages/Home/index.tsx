4a933eb1dd5d23edac583e60022d671b457ff47b
gdegato <rosele.tst@gmail.com>
2022-12-17 12:13:43 -0300

    Adicionando componente PrivateRoute para rotas autorizadas

--- react-intro/src/pages/Home/index.tsx
+++ react-intro/src/pages/Home/index.tsx
@@ -3,29 +3,27 @@ import { ReactComponent as MainImage } from '../../assets/images/main-image.svg'
 import { isAuthenticated } from 'util/requests';
 import { Link } from 'react-router-dom';
 import './styles.css';
-const Home = () => {
-  return (
-    <>
-      <div className="home-container">
-        <h1>{isAuthenticated() ? 'autenticado' : 'Não autenticado'}</h1>
-        <div className="base-card home-card">
-          <div className="home-content-container">
-            <div>
-              <h1>Conheça o melhor catálogo de produtos</h1>
-              <p>
-                Ajudaremos você a encontrar os melhores produtos disponíveis no
-                mercado.
-              </p>
-            </div>
-            <Link to="/products"></Link>
-            <ButtonIcon text="Inicie agora a sua busca" />
+
+const Home = () => (
+  <>
+    <div className="home-container">
+      <div className="base-card home-card">
+        <div className="home-content-container">
+          <div>
+            <h1>Conheça o melhor catálogo de produtos</h1>
+            <p>
+              Ajudaremos você a encontrar os melhores produtos disponíveis no
+              mercado.
+            </p>
           </div>
-          <div className="home-image-container">
-            <MainImage />
-          </div>
+          <Link to="/products"></Link>
+          <ButtonIcon text="Inicie agora a sua busca" />
+        </div>
+        <div className="home-image-container">
+          <MainImage />
         </div>
       </div>
-    </>
-  );
-};
+    </div>
+  </>
+);
 export default Home;
--- react-intro/src/util/requests.ts
+++ react-intro/src/util/requests.ts
@@ -100,3 +100,4 @@ export const isAuthenticated = () : boolean => {
     const tokenData = getTokenData();
     return(tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
 }
+

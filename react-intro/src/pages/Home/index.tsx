29af241bf556bed9f970ed76b20ed551d04fa753
gdegato <rosele.tst@gmail.com>
2023-01-15 10:46:17 -0300

    Implementando a função hasAnyRoles

--- react-intro/src/pages/Home/index.tsx
+++ react-intro/src/pages/Home/index.tsx
@@ -1,29 +1,32 @@
 import ButtonIcon from '../../components/ButtonIcon';
 import { ReactComponent as MainImage } from '../../assets/images/main-image.svg';
-import { isAuthenticated } from 'util/requests';
+import { hasAnyRoles, isAuthenticated } from 'util/requests';
 import { Link } from 'react-router-dom';
 import './styles.css';
 
-const Home = () => (
-  <>
-    <div className="home-container">
-      <div className="base-card home-card">
-        <div className="home-content-container">
-          <div>
-            <h1>Conheça o melhor catálogo de produtos</h1>
-            <p>
-              Ajudaremos você a encontrar os melhores produtos disponíveis no
-              mercado.
-            </p>
+const Home = () => {
+  return (
+    <>
+      <div className="home-container">
+        <h1>Resultado = {hasAnyRoles(['ROLE_ADMIN', 'ROLE_OPERATOR'])? 'sim' : 'não'}</h1>
+        <div className="base-card home-card">
+          <div className="home-content-container">
+            <div>
+              <h1>Conheça o melhor catálogo de produtos</h1>
+              <p>
+                Ajudaremos você a encontrar os melhores produtos disponíveis no
+                mercado.
+              </p>
+            </div>
+            <Link to="/products"></Link>
+            <ButtonIcon text="Inicie agora a sua busca" />
           </div>
-          <Link to="/products"></Link>
-          <ButtonIcon text="Inicie agora a sua busca" />
-        </div>
-        <div className="home-image-container">
-          <MainImage />
+          <div className="home-image-container">
+            <MainImage />
+          </div>
         </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 export default Home;

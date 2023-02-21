04436968a2df8bb79eaa4ef22d6964734129a030
gdegato <rosele.tst@gmail.com>
2023-02-21 18:15:03 -0300

    Form buttons styling

--- react-intro/src/pages/Admin/Products/Form/index.tsx
+++ react-intro/src/pages/Admin/Products/Form/index.tsx
@@ -6,23 +6,35 @@ const Form = () => {
       <div className="base-card product-crud-form-card">
         <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
         <form action="">
-          <div className="row">
-            <div className="col-lg-6">
-              <input type="text" className="base-input form-control" />
-              <input type="text" className="base-input form-control" />
-              <input type="text" className="base-input form-control" />
+          <div className="row product-crud-input-container">
+            <div className="col-lg-6 product-crud-inputs-left-container">
+              <div className="margin-bottom-30">
+                <input type="text" className="base-input form-control" />
+              </div>
+              <div className="margin-bottom-30">
+                <input type="text" className="base-input form-control" />
+              </div>
+              <div>
+                <input type="text" className="base-input form-control" />
+              </div>
             </div>
             <div className="col-lg-6">
-              <textarea
-                name=""
-                rows={10}
-                className="base-input form-control"
-              ></textarea>
+              <div>
+                <textarea
+                  name=""
+                  rows={10}
+                  className="form-control base-input h-auto"
+                />
+              </div>
             </div>
           </div>
-          <div>
-            <button className="btn btn-outline-danger">CANCELAR</button>
-            <button className="btn btn-primary">SALVAR</button>
+          <div className="product-crud-buttons-container">
+            <button className="btn btn-outline-danger product-crud-button">
+              CANCELAR
+            </button>
+            <button className="btn btn-primary product-crud-button text-white">
+              SALVAR
+            </button>
           </div>
         </form>
       </div>
